0151090c3535116ece5add9608a24be7a6a67a27
Jonathan <jdmoore3@ualr.edu>
2015-03-05 14:32:58 -0600

    Added blueimg gallery.

---
+++ js/main.js
@@ -0,0 +1,51 @@
+﻿/* Main JS */
+
+/* global blueimp, $ */
+
+$(function () {
+    'use strict';
+
+    // Initialize the Gallery as video carousel:
+    blueimp.Gallery([
+        {
+            title: 'Arkansas Aerials Spring',
+            href: 'https://www.youtube.com/watch?v=MP6EGIipkqg',
+            type: 'text/html',
+            youtube: 'MP6EGIipkqg',
+            poster: 'https://img.youtube.com/vi/MP6EGIipkqg/maxresdefault.jpg'
+        },
+        {
+            title: 'Arkansas Aerials Summer',
+            href: 'https://www.youtube.com/watch?v=hQtW4vWrjvY',
+            type: 'text/html',
+            youtube: 'hQtW4vWrjvY',
+            poster: 'https://img.youtube.com/vi/hQtW4vWrjvY/maxresdefault.jpg'
+        },
+        {
+            title: 'Arkansas Aerials Fall',
+            href: 'https://www.youtube.com/watch?v=PEU43TD3wD8',
+            type: 'text/html',
+            youtube: 'PEU43TD3wD8',
+            poster: 'https://img.youtube.com/vi/PEU43TD3wD8/maxresdefault.jpg'
+        },
+        {
+            title: 'Arkansas Aerials Winter',
+            href: 'https://www.youtube.com/watch?v=OYPfFJqgV5A',
+            type: 'text/html',
+            youtube: 'OYPfFJqgV5A',
+            poster: 'https://img.youtube.com/vi/OYPfFJqgV5A/maxresdefault.jpg'
+        },
+        {
+            title: 'Arkansas Aerials Dealership',
+            href: 'https://www.youtube.com/watch?v=wzH5GyVRPWA',
+            type: 'text/html',
+            youtube: 'wzH5GyVRPWA',
+            poster: 'https://img.youtube.com/vi/wzH5GyVRPWA/maxresdefault.jpg'
+        }
+
+    ], {
+        container: '#blueimp-video-carousel',
+        carousel: true
+    });
+
+});
